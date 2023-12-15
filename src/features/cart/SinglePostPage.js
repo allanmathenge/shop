1a8b10b7c2b4bd6ab07928d7efab19498b1f8c53
allan <allanmathenge2@gmail.com>
2023-12-15 11:16:16 +0300

    commit

--- src/features/cart/SinglePostPage.js
+++ src/features/cart/SinglePostPage.js
@@ -1,12 +1,17 @@
 import { useSelector } from "react-redux";
 import { selectItemById } from "./cartSlice";
+import { AddCart } from "../../assets/icons";
 import TimeAgo from "./TimeAgo";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../cart/cartSlice";
+import CartNavbar from "./CartNavbar";
 
 import { useParams } from "react-router-dom";
 
 const SinglePostPage = () => {
   //Retrieving itemId from url
 
+  const dispatch = useDispatch();
   const { id } = useParams();
   const item = useSelector((state) => selectItemById(state, parseInt(id)));
 
@@ -20,12 +25,23 @@ const SinglePostPage = () => {
 
   return (
     <article className="page">
-      <h2>{item.title}</h2>
+      <CartNavbar />
+      <h3>{item.title}</h3>
       <img src={item.image} width="100px" alt={item.title} />
       <h4>${item.price}</h4>
-      <h4>
-        <TimeAgo timestamp={item.date} />
-      </h4>
+      <div className="page-shop">
+        <h4>
+          <TimeAgo timestamp={item.date} />
+        </h4>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(addToCart({ id }));
+          }}
+        >
+          <AddCart />
+        </button>
+      </div>
     </article>
   );
 };
